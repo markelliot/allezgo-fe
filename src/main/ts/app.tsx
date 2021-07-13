551f6875ed5dd273a915b347f87d0cc12f8a70d4
aditkumar <aditkumar@gmail.com>
2021-07-13 15:20:12 -0400

    Add Google Analytics (#1)
    * add navbar
    * add google analytics

--- src/main/ts/app.tsx
+++ src/main/ts/app.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ReactGA from 'react-ga';
 
 import './app.scss'
+import { Nav } from './nav'
 import { Login } from './login'
 import { PelotonToGarmin } from './pelotonToGarmin'
 
+
 // render root element
+ReactGA.initialize('UA-202157985-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
+    <>
+    <Nav/>
     <Router>
         <Switch>
             <Route path="/peloton-to-garmin">
@@ -17,6 +25,7 @@ ReactDOM.render(
                 <Login />
             </Route>
         </Switch>
-    </Router>,
+    </Router>
+    </>,
     document.getElementById('root'),
 )
